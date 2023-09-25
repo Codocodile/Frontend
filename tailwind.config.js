6d3a42d0a84018a8e6502705e295b8297a14d0a7
saman-re <68374661+saman-re@users.noreply.github.com>
2023-09-25 23:56:14 +0330

    style: change tailwind colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 import withMT from "@material-tailwind/react/utils/withMT";
+import colors from 'tailwindcss/colors'
 
 export default withMT({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors
+    },
   },
   plugins: [],
 })
