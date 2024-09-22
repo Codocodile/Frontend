0227cf6feb14a5ac7734f5e59177195fe239e28e
Moeein Aali <moeeeinaali@gmail.com>
2024-09-22 13:15:59 +0330

    Fix tailwind.config Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 import withMT from "@material-tailwind/react/utils/withMT";
-import colors from 'tailwindcss/colors'
+import {sky, stone, neutral, gray, slate} from 'tailwindcss/colors';
 
 export default withMT({
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                sky,
+                stone,
+                neutral,
+                gray,
+                slate,
+            }
+        },
     },
-  },
-  plugins: [],
-})
+    plugins: [],
+});
