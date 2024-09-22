87083266eff7f5d342faecf99ca85b17a406d745
Moeein Aali <moeeeinaali@gmail.com>
2024-09-22 13:20:49 +0330

    Navigate wrong Routes to /Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import Landing from "./components/landing/Landing.tsx";
 import Panel from "./components/panel/Panel.tsx";
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes, Navigate} from "react-router-dom";
 import SignIn from "./components/signIn/SignIn.tsx";
-import { urls } from "./global/Variables.ts";
+import {urls} from "./global/Variables.ts";
 import Profile from "./components/profile/Profile.tsx";
 import Team from "./components/team/Team.tsx";
 import PasswordReset from "./components/password-reset/PasswordReset.tsx";
@@ -11,21 +11,22 @@ import ForgetPassword from "./components/password-reset/ForgetPassword.tsx";
 import SignUp from "./components/signUp/SignUp.tsx";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route index element={<Landing />} />
-        <Route path={urls.signIn} element={<SignIn />} />
-        <Route path={urls.signUp} element={<SignUp />} />
-        <Route path={urls.passwordReset} element={<PasswordReset />} />
-        <Route path={urls.forgetPassword} element={<ForgetPassword />} />
-        <Route path={urls.panel} element={<Panel />}>
-          <Route path="profile" element={<Profile />} />
-          <Route path="team" element={<Team />} />
-        </Route>
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route index element={<Landing/>}/>
+                <Route path={urls.signIn} element={<SignIn/>}/>
+                <Route path={urls.signUp} element={<SignUp/>}/>
+                <Route path={urls.passwordReset} element={<PasswordReset/>}/>
+                <Route path={urls.forgetPassword} element={<ForgetPassword/>}/>
+                <Route path={urls.panel} element={<Panel/>}>
+                    <Route path="profile" element={<Profile/>}/>
+                    <Route path="team" element={<Team/>}/>
+                </Route>
+                <Route path={urls.wildCard} element={<Navigate to={"/"}/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/global/Variables.ts
+++ src/global/Variables.ts
@@ -1,13 +1,14 @@
 export const urls = {
-  landing: '/',
-  signIn: '/sign-in',
-  signUp: '/sign-up',
-  forgetPassword: '/forget-password',
-  passwordReset: '/password-reset/:token',
-  panel: '/panel',
-  profile: '/panel/profile',
-  team: '/panel/team',
+    landing: '/',
+    signIn: '/sign-in',
+    signUp: '/sign-up',
+    forgetPassword: '/forget-password',
+    passwordReset: '/password-reset/:token',
+    panel: '/panel',
+    profile: '/panel/profile',
+    team: '/panel/team',
+    wildCard: "*"
 }
 
-export const API_URL = 'https://codocodile.com/api'
-// export const API_URL = 'http://localhost:8000'
+// export const API_URL = 'https://codocodile.com/api'
+export const API_URL = 'http://localhost:8000'
