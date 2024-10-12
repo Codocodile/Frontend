80590770f1313fc781cc2dc3c5cb35944dadc2f8
Moeein Aali <moeeeinaali@gmail.com>
2024-10-12 13:38:30 +0330

    Add TimeLine.ts,Staff.ts

---
+++ src/global/Staff.ts
@@ -0,0 +1,35 @@
+export interface IStaff {
+    name: string,
+    title: string,
+    image: string,
+    linkedin: string,
+    github: string,
+    email: string
+}
+
+export const staff: IStaff[] = [
+    {
+        name: "Ali Shahali",
+        title: "Head of Event",
+        image: "assets/staff/shahali.jpg",
+        linkedin: "",
+        github: "",
+        email: "",
+    },
+    {
+        name: "Ali Salesi",
+        title: "Head of Technical",
+        image: "assets/staff/salesi.jpg",
+        linkedin: "",
+        github: "",
+        email: "",
+    },
+    {
+        name: "Emad Emam Jome",
+        title: "Head of Social",
+        image: "assets/staff/emamjome.jpg",
+        linkedin: "",
+        github: "",
+        email: "",
+    },
+];
---
+++ src/global/TimeLine.ts
@@ -0,0 +1,22 @@
+export const timeline = [
+    {
+        date: "22 Mehr",
+        title: "Registeration Starts.",
+        description: "ثبت‌نام انفرادی در سایت شروع می‌شود. تیم‌بندی برای مسابقه در روزهای آتی انجام خواهد شد."
+    },
+    {
+        date: "26 Mehr - 2 Aban",
+        title: "Algorithmic Workshops",
+        description: "??????"
+    },
+    {
+        date: "4 Aban",
+        title: "Selection Contest",
+        description: "مسابقه انتخابی به صورت مجازی در روز ۴ آبان برگزار می‌شود و تیم‌های راه یافته به مرحله نهایی حضوری از نتیجه این مسابقه انتخاب می‌شود."
+    },
+    {
+        date: "2 Azar",
+        title: "Final Contest",
+        description: "مسابقات نهایی رویداد در یک روز به صورت حضوری در دانشگاه شریف برگزار می‌شود. جوایز و رتبه‌های نهایی پس از این رقابت مشخص خواهد شد."
+    },
+];
